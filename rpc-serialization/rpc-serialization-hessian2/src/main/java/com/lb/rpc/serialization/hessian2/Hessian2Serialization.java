7faecd0b40a43c743a01a5b1fe998f043dd4493d
ZhiYu <RainSoul1120@outlook.com>
2025-07-01 17:21:31 +0800

    基于SPI扩展Hessian2序列化与反序列化机制

---
+++ rpc-serialization/rpc-serialization-hessian2/src/main/java/com/lb/rpc/serialization/hessian2/Hessian2Serialization.java
@@ -0,0 +1,77 @@
+package com.lb.rpc.serialization.hessian2;
+
+import com.caucho.hessian.io.Hessian2Input;
+import com.caucho.hessian.io.Hessian2Output;
+import com.lb.rpc.common.exception.SerializerException;
+import com.lb.rpc.serialization.api.Serialization;
+import com.lb.rpc.spi.annotation.SPIClass;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+@SPIClass
+public class Hessian2Serialization implements Serialization {
+    private final Logger logger = LoggerFactory.getLogger(Hessian2Serialization.class);
+
+    @Override
+    public <T> byte[] serialize(T obj) {
+        logger.info("execute hessian2 serialize...");
+        if (obj == null) {
+            throw new SerializerException("serialize object is null");
+        }
+        byte[] result = new byte[0];
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        Hessian2Output hessian2Output = new Hessian2Output(byteArrayOutputStream);
+        try {
+            hessian2Output.startMessage();
+            hessian2Output.writeObject(obj);
+            hessian2Output.flush();
+            hessian2Output.completeMessage();
+            result = byteArrayOutputStream.toByteArray();
+        } catch (IOException e) {
+            throw new SerializerException(e.getMessage(), e);
+        } finally {
+            try {
+                if (hessian2Output != null) {
+                    hessian2Output.close();
+                    byteArrayOutputStream.close();
+                }
+            } catch (IOException e) {
+                throw new SerializerException(e.getMessage(), e);
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public <T> T deserialize(byte[] data, Class<T> cls) {
+        logger.info("execute hessian2 deserialize...");
+        if (data == null) {
+            throw new SerializerException("deserialize data is null");
+        }
+        ByteArrayInputStream byteInputStream = new ByteArrayInputStream(data);
+        Hessian2Input hessian2Input = new Hessian2Input(byteInputStream);
+        T object = null;
+        try {
+            hessian2Input.startMessage();
+            object = (T) hessian2Input.readObject();
+            hessian2Input.completeMessage();
+        } catch (IOException e) {
+            throw new SerializerException(e.getMessage(), e);
+        } finally {
+            try {
+                if (null != hessian2Input) {
+                    hessian2Input.close();
+                    byteInputStream.close();
+                }
+            } catch (IOException e) {
+                throw new SerializerException(e.getMessage(), e);
+            }
+            return object;
+        }
+
+    }
+}
