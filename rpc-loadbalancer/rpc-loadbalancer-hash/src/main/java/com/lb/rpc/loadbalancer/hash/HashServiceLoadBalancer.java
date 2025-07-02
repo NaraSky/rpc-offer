b423754dabc498751f84803ac8a405c19eb48138
ZhiYu <RainSoul1120@outlook.com>
2025-07-02 19:13:06 +0800

    基于SPI扩展Hash算法负载均衡策略

---
+++ rpc-loadbalancer/rpc-loadbalancer-hash/src/main/java/com/lb/rpc/loadbalancer/hash/HashServiceLoadBalancer.java
@@ -0,0 +1,23 @@
+package com.lb.rpc.loadbalancer.hash;
+
+import com.lb.rpc.loadbalancer.api.ServiceLoadBalancer;
+import com.lb.rpc.spi.annotation.SPIClass;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+@SPIClass
+public class HashServiceLoadBalancer<T> implements ServiceLoadBalancer<T> {
+    private final Logger logger = LoggerFactory.getLogger(HashServiceLoadBalancer.class);
+
+    @Override
+    public T select(List<T> servers, int hashCode) {
+        logger.info("基于Hash算法的负载均衡策略...");
+        if (servers == null || servers.isEmpty()) {
+            return null;
+        }
+        int index = Math.abs(hashCode) % servers.size();
+        return servers.get(index);
+    }
+}
--- rpc-test/rpc-test-consumer/rpc-test-consumer-native/src/main/java/com/lb/test/consumer/RpcConsumerNativeTest.java
+++ rpc-test/rpc-test-consumer/rpc-test-consumer-native/src/main/java/com/lb/test/consumer/RpcConsumerNativeTest.java
@@ -39,7 +39,8 @@ public class RpcConsumerNativeTest {
         // rpcClient = new RpcClient("127.0.0.1:2181", "zookeeper", "random","asm","1.0.0", "zhiyu", "protostuff", 3000, false, false);
         // rpcClient = new RpcClient("127.0.0.1:2181", "zookeeper", "randomweight","asm","1.0.0", "zhiyu", "protostuff", 3000, false, false);
         // rpcClient = new RpcClient("127.0.0.1:2181", "zookeeper", "robin","asm","1.0.0", "zhiyu", "protostuff", 3000, false, false);
-        rpcClient = new RpcClient("127.0.0.1:2181", "zookeeper", "robinweight","asm","1.0.0", "zhiyu", "protostuff", 3000, false, false);
+        // rpcClient = new RpcClient("127.0.0.1:2181", "zookeeper", "robinweight","asm","1.0.0", "zhiyu", "protostuff", 3000, false, false);
+        rpcClient = new RpcClient("127.0.0.1:2181", "zookeeper", "hash","asm","1.0.0", "zhiyu", "protostuff", 3000, false, false);
     }
 
     @Test
