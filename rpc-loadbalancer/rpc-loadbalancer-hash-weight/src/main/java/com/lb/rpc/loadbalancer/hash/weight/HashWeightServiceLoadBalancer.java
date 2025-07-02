14b8dcd0da6fdc3628c9e494c81aac471a825e87
ZhiYu <RainSoul1120@outlook.com>
2025-07-02 19:30:08 +0800

    基于SPI扩展加权Hash算法负载均衡策略

---
+++ rpc-loadbalancer/rpc-loadbalancer-hash-weight/src/main/java/com/lb/rpc/loadbalancer/hash/weight/HashWeightServiceLoadBalancer.java
@@ -0,0 +1,28 @@
+package com.lb.rpc.loadbalancer.hash.weight;
+
+import com.lb.rpc.loadbalancer.api.ServiceLoadBalancer;
+import com.lb.rpc.spi.annotation.SPIClass;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+@SPIClass
+public class HashWeightServiceLoadBalancer<T> implements ServiceLoadBalancer<T> {
+    private final Logger logger = LoggerFactory.getLogger(HashWeightServiceLoadBalancer.class);
+
+    @Override
+    public T select(List<T> servers, int hashCode) {
+        logger.info("基于加权Hash算法的负载均衡策略...");
+        if (servers == null || servers.isEmpty()) {
+            return null;
+        }
+        hashCode = Math.abs(hashCode);
+        int count = hashCode % servers.size();
+        if (count <= 0) {
+            count = servers.size();
+        }
+        int index = hashCode % count;
+        return servers.get(index);
+    }
+}
